9505fc5c9d717e67de7ac4570b31440cebbbe8f2
FelpinhoDaMassa <91645982+FelpinhoDaMassa@users.noreply.github.com>
2021-09-30 15:42:52 -0200

    jogo da velha v3
    30/09/2021
    Estou tentando implementar o resultado "velha", mas nao estou conseguindo acabar com o loop do jogo

--- jogodavelha.py
+++ jogodavelha.py
@@ -1,4 +1,5 @@
 from random import randint
+from time import sleep
 
 def titulo(msg = str):
     n = len(msg) + 4
@@ -8,15 +9,26 @@ def titulo(msg = str):
 
 def mostrar(lista = []):
     print('=-'*15)
-    for coluna in range (0, 3):
-        for linha in range(0, 3):
+    for coluna in range (3):
+        for linha in range(3):
             print(f'[{lista[coluna][linha]:^5}]', end='')
         print()
 
-
-jogo = True
-contador = 1
-jogador_linha = jogador_coluna = 0
+def checar_Velha(lista = [], contador = 0):
+    '''
+    lista: matriz/tabuleiro do jogo
+    alive: boolean que continua o jogo
+    contador: contador que, ao chegar em 9 (total de espacos usados) acaba o jogo'''
+    contador = 0
+    global jogo
+    for linha in range(3):
+        for coluna in range(3):
+            if lista[linha][coluna] != '':
+                contador +=1
+    if contador == 9:
+        print('DEU VELHA')
+        jogo = False
+    # print(contador)
 
 # CRIA O TABULEIRO
 tabuleiro = []
@@ -25,11 +37,16 @@ for i in range(3):
     for j in range(3):
         tabuleiro[i].append('')
 
+# VARIAVEIS GLOBAIS
+jogo = True
+jogador_linha = jogador_coluna = 0
 
+# LOOP DO JOGO
 titulo('JOGO DA VELHA')
 while jogo == True:
-    if contador == 1:
-        mostrar(tabuleiro)
+    mostrar(tabuleiro)
+    checar_Velha(tabuleiro)
+    print(jogo)
 
     #JOGADA DO JOGADOR
     jogador_linha = int(input('Linha da sua jogada: '))
@@ -41,9 +58,13 @@ while jogo == True:
         jogador_linha = int(input('Linha da sua jogada: '))
         jogador_coluna = int(input('Coluna da jogada: '))
     tabuleiro[jogador_linha-1][jogador_coluna-1] = 'X'
-    
+
+    mostrar(tabuleiro)
+    checar_Velha(tabuleiro)
+    print(jogo)
+    sleep(1)
+
     #JOGADA DO CPU
-    #tabuleiro[cpu_linha][cpu_coluna] = 'O'
     cpu_linha = randint(0,2)
     cpu_coluna = randint(0,2)
 
@@ -52,8 +73,6 @@ while jogo == True:
         cpu_linha = randint(0,2)
         cpu_coluna = randint(0,2)
     tabuleiro[cpu_linha][cpu_coluna] = 'O'
-    
-    mostrar(tabuleiro)
 
 print('Acabou')
 
